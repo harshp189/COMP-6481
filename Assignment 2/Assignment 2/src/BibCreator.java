d5e95faac194b9d154eddc4a615d67806184700f
Harsh Patel <harshp1898@gmail.com>
2022-03-08 21:26:18 -0500

    Created the openFile method to open all the files to be written in three formats

--- Assignment 2/Assignment 2/src/BibCreator.java
+++ Assignment 2/Assignment 2/src/BibCreator.java
@@ -3,5 +3,28 @@ import java.util.Scanner;
 
 public class BibCreator {
 
+    /**
+     * This method will open all the files in three formats to be written
+     * @param printWriters array of printwriters
+     */
 
+    private static void openAllFiles(PrintWriter[][] printWriters) {
+        for (int i = 0; i < 10; i++) {
+            try {
+                printWriters[i][0] = new PrintWriter(new FileOutputStream("ACM" + (i + 1) + ".json"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                printWriters[i][1] = new PrintWriter(new FileOutputStream("IEEE" + (i + 1) + ".json"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                printWriters[i][2] = new PrintWriter(new FileOutputStream("NJ" + (i + 1) + ".json"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
